d1f4e961ea0f2c58a3f6a190af8b8f0e67cb27d0
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-03-02 19:43:13 +0300

    add write to file

--- src/main/java/by/matrosov/appl/HltvParser.java
+++ src/main/java/by/matrosov/appl/HltvParser.java
@@ -4,18 +4,17 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class HltvParser {
     public static void main(String[] args) throws IOException {
         List<String> matchesList = new ArrayList<>();
         List<Double> firstTeamOdds = new ArrayList<>();
         List<Double> secondTeamOdds = new ArrayList<>();
-        List<String> oddsList = new ArrayList<>();
         Map<String,String> result = new HashMap<>();
 
         Document document = Jsoup.connect("https://www.hltv.org").get();
@@ -37,62 +36,58 @@ public class HltvParser {
                     if (element.className().contains("_egb")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
-                        //oddsList.add("EGB " + odds1 + "-" + odds2);
-                        //result.put(teamsName, odds1 + "-" + odds2);
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.05);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.05);
                     } else if (element.className().contains("_betway")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
-                        //result.put(teamsName, odds1 + "-" + odds2);
-                        //oddsList.add("betway " + odds1 + "-" + odds2);
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.042);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.042);
                     } else if (element.className().contains("_lootbet")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.035);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.035);
                     }else if (element.className().contains("day geoprovider_ggbet")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.041);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.041);
                     }else if (element.className().contains("day geoprovider_thunderpick")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
+                        firstTeamOdds.add(Double.parseDouble(odds1));  //?
                         secondTeamOdds.add(Double.parseDouble(odds2));
                     }else if (element.className().contains("_csgopositive")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
+                        firstTeamOdds.add(Double.parseDouble(odds1)); //?
                         secondTeamOdds.add(Double.parseDouble(odds2));
                     }else if (element.className().contains("_bet365")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.04);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.04);
                     }else if (element.className().contains("_bet188")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.031);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.031    );
                     }else if (element.className().contains("day geoprovider_1xbet")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.027);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.027);
                     }else if (element.className().contains("day geoprovider_esporbet")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.031);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.031);
                     }else if (element.className().contains("day geoprovider_pinnacle")){
                         String odds1 = element.child(1).text();
                         String odds2 = element.child(3).text();
-                        firstTeamOdds.add(Double.parseDouble(odds1));
-                        secondTeamOdds.add(Double.parseDouble(odds2));
+                        firstTeamOdds.add(Double.parseDouble(odds1) - 0.024);
+                        secondTeamOdds.add(Double.parseDouble(odds2) - 0.024);
                     }
                 }catch (Exception ignore){
 
@@ -113,10 +108,17 @@ public class HltvParser {
             double secondSumResult = secondSum / secondTeamOdds.size();
 
             result.put(teamsName, String.format("%.2f", firstSumResult) + " and " + String.format("%.2f", secondSumResult));
-
-            for (Map.Entry entry : result.entrySet()) {
-                System.out.println(entry.getKey() + ", " + entry.getValue());
-            }
+            firstTeamOdds.clear();
+            secondTeamOdds.clear();
         }
+
+
+        File file = new File("D:\\file.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        for (Map.Entry entry : result.entrySet()) {
+            writer.write(entry.getKey() + ", " + entry.getValue());
+            writer.newLine();
+        }
+        writer.close();
     }
 }
