3b468bb8ff6581bcc1bb1d5b170c778b196c36e3
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-03-21 15:57:37 +0300

    add '-vs-' checker

--- src/main/java/by/matrosov/appl/HltvParser.java
+++ src/main/java/by/matrosov/appl/HltvParser.java
@@ -133,8 +133,10 @@ public class HltvParser {
         File file = new File("D:\\file.txt");
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         for (Map.Entry entry : finalResult.entrySet()) {
-            writer.write(entry.getKey() + "! " + entry.getValue());
-            writer.newLine();
+            if (entry.getKey().toString().contains("-vs-")){
+                writer.write(entry.getKey() + "! " + entry.getValue());
+                writer.newLine();
+            }
         }
         writer.close();
     }
